67f6665c8aa041296941ae3763337eb2c4757b60
Martin Daser <martin.daser@web.de>
2012-03-23 20:01:07 +0100

    Repeat video via CompletionListener

--- src/net/mdaser/vloop/VloopActivity.java
+++ src/net/mdaser/vloop/VloopActivity.java
@@ -1,14 +1,35 @@
+/*
+ * (c) Copyright 2012 by Martin Daser
+ */
 package net.mdaser.vloop;
 
-import android.R.string;
 import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
+import android.media.MediaPlayer;
 
 public class VloopActivity extends Activity {
+	
+	class CompletionHandler implements MediaPlayer.OnCompletionListener
+	{
+		@Override
+		public void onCompletion(MediaPlayer mp) {
+			
+			// show a toast ... restarting
+			Context context = getApplicationContext();
+			Toast toast = Toast.makeText(context, R.string.Restart, Toast.LENGTH_SHORT);
+			toast.show();
+			
+			// restart the media player
+			mp.start();
+		}
+    }
+	
+	
     /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +41,12 @@ public class VloopActivity extends Activity {
         videoView.setMediaController(mc);
         
         String path = "android.resource://net.mdaser.vloop/" + R.raw.video;
-        videoView.setVideoURI(Uri.parse(path));
-        //      "http://www.androidbook.com/akc/filestorage/android/documentfiles/3389/movie.mp4"));
-              
+        // "http://www.androidbook.com/akc/filestorage/android/documentfiles/3389/movie.mp4"));
+        videoView.setVideoURI(Uri.parse(path));             
         // videoView.setVideoPath("/sdcard/movie.mp4");
+        
+        videoView.setOnCompletionListener(new CompletionHandler());
+        
         videoView.requestFocus();
         videoView.start();
     }
