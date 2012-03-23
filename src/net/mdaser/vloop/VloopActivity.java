984e7c363079d5773374e449d56f871e84f9ba03
Martin Daser <martin.daser@web.de>
2012-03-23 18:23:18 +0100

    Initial commit; code generated by Android Plugin

---
+++ src/net/mdaser/vloop/VloopActivity.java
@@ -0,0 +1,13 @@
+package net.mdaser.vloop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VloopActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
