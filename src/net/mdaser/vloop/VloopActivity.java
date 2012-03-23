27f679ddfaa6b9361dfc66dbdfbea5275ee9cb73
Martin Daser <martin.daser@web.de>
2012-03-23 19:20:40 +0100

    Play video included in package

--- src/net/mdaser/vloop/VloopActivity.java
+++ src/net/mdaser/vloop/VloopActivity.java
@@ -1,8 +1,10 @@
 package net.mdaser.vloop;
 
+import android.R.string;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -10,14 +12,16 @@ public class VloopActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); 
         this.setContentView(R.layout.main);
 
         VideoView videoView = (VideoView)this.findViewById(R.id.videoView);
         MediaController mc = new MediaController(this);
         videoView.setMediaController(mc);
-        videoView.setVideoURI(Uri.parse(
-              "http://www.androidbook.com/akc/filestorage/android/documentfiles/3389/movie.mp4"));
+        
+        String path = "android.resource://net.mdaser.vloop/" + R.raw.video;
+        videoView.setVideoURI(Uri.parse(path));
+        //      "http://www.androidbook.com/akc/filestorage/android/documentfiles/3389/movie.mp4"));
               
         // videoView.setVideoPath("/sdcard/movie.mp4");
         videoView.requestFocus();
